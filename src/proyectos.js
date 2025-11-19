f04d7becc89aef2fef7214727836f9dce2823d4d
Oziel David Canul Xool <al066998@uacam.mx>
2025-11-19 10:51:53 -0600

    mi portafolio

--- src/proyectos.js
+++ src/proyectos.js
@@ -2,38 +2,38 @@ export default [
   {
     titulo: "Gato (Tic Tac Toe)",
     descripcion: "Juego clásico del gato hecho con JavaScript.",
-    imagen: "/gato.png",
+    imagen: "public/gato.png",
     url: "https://al066998-art.github.io/Tic-Tac-Toe-juego/  ",
   },
   {
     titulo: "Login Form",
     descripcion: "Formulario con operaciones basicas para manipular una base de datos.",
-    imagen: "/Form.png",
+    imagen: "public/Form.png",
     url: "https://al066998-art.github.io/Formulario_CRUD/",
   },
   {
     titulo: "Escapa del laberinto",
     descripcion: "Mini juego sencillo hecho en JavaScript.",
-    imagen: "/Escapa.png",
+    imagen: "public/Escapa.png",
     url: "https://al066998-art.github.io/escapa_de_beyonce/",
   },
 
   {
     titulo: "Cheat Sheet",
     descripcion: "Acordeón con los principales elementos HTML.",
-    imagen: "/cheat.png",
+    imagen: "public/cheat.png",
     url: "https://al066998-art.github.io/chat_sheet/",
   },
   {
     titulo: "Mi página web",
     descripcion: "Página web estilo red social responsiva.",
-    imagen: "/web.png",
+    imagen: "public/web.png",
     url: "https://al066998-art.github.io/paginas_dise-o_web/",
   },
   {
     titulo: "Página inclusiva",
     descripcion: "Diseño web inclusivo para personas con discapacidad visual.",
-    imagen: "/web-inclusive.png",
+    imagen: "public/web-inclusive.png",
     url: "https://al066998-art.github.io/e-comerce/",
   },
 ];
