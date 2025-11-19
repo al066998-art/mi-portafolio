16eb6a5c740df76c1b293a7dbb2a1fbdc3317223
Oziel David Canul Xool <al066998@uacam.mx>
2025-11-19 11:59:28 -0600

    mi portafolio

--- src/proyectos.js
+++ src/proyectos.js
@@ -8,32 +8,32 @@ export default [
   {
     titulo: "Login Form",
     descripcion: "Formulario con operaciones basicas para manipular una base de datos.",
-    imagen: process.env.PUBLIC_URL + "public/Form.png",
+    imagen: process.env.PUBLIC_URL + "/Form.png",
     url: "https://al066998-art.github.io/Formulario_CRUD/",
   },
   {
     titulo: "Escapa del laberinto",
     descripcion: "Mini juego sencillo hecho en JavaScript.",
-    imagen: process.env.PUBLIC_URL + "public/Escapa.png",
+    imagen: process.env.PUBLIC_URL + "/Escapa.png",
     url: "https://al066998-art.github.io/escapa_de_beyonce/",
   },
 
   {
     titulo: "Cheat Sheet",
     descripcion: "Acordeón con los principales elementos HTML.",
-    imagen: process.env.PUBLIC_URL + "public/cheat.png",
+    imagen: process.env.PUBLIC_URL + "/cheat.png",
     url: "https://al066998-art.github.io/chat_sheet/",
   },
   {
     titulo: "Mi página web",
     descripcion: "Página web estilo red social responsiva.",
-    imagen: process.env.PUBLIC_URL + "public/web.png",
+    imagen: process.env.PUBLIC_URL + "/web.png",
     url: "https://al066998-art.github.io/paginas_dise-o_web/",
   },
   {
     titulo: "Página inclusiva",
     descripcion: "Diseño web inclusivo para personas con discapacidad visual.",
-    imagen: process.env.PUBLIC_URL +"public/web-inclusive.png",
+    imagen: process.env.PUBLIC_URL +"/web-inclusive.png",
     url: "https://al066998-art.github.io/e-comerce/",
   },
 ];
